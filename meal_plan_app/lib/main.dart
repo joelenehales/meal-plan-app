b82231c70e374e73688053672c247df0e5eb086d
joelenehales <joelenehales@bell.net>
2024-07-21 21:31:41 -0400

    Start on recipes database

--- meal_plan_app/lib/main.dart
+++ meal_plan_app/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MealPlanner());
@@ -92,5 +97,54 @@ class _HomePageState extends State<HomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class Recipe {
+  final int? id; // ? means can be null
+  final String name;
+
+  Recipe({this.id, required this.name});
+
+  factory Recipe.fromMap(Map<String, dynamic> json) => Recipe(
+        id: json['id'],
+        name: json['name'],
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  // If database does not exist, initialize database. Otherwise, use existing database.
+  static Database? _database;
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  // Opens database
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'recipes.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  // Run when opening a database that does not yet exist. Creates all needed tables
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE recipes(
+      id INTEGER PRIMARY KEY,
+      name TEXT
+    )
+    ''');
   }
 }
